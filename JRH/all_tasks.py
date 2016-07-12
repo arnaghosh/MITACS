8ba721eafe1b73069285354b8a041de6be220daf
Arna Ghosh <arnatubaikgp@gmail.com>
2016-07-12 19:23:22 -0400

    updated to have timeAfterExercise in filename

--- JRH/all_tasks.py
+++ JRH/all_tasks.py
@@ -2,6 +2,7 @@ from gripperMax import gripperMax
 from holdTask import holdTask
 from isometricTask import isometricTask
 from motorLearningTask import motorLearningTask
+from presentCross import presentCross
 from task_GUI_prog import Ui_MainWindow
 from PyQt4 import QtGui
 import numpy as np
@@ -9,6 +10,7 @@ import cv2, sys, os, time, win32api, wx, threading, win32gui
 import win32ui, win32con, re, pywinauto, win32com.client
 from libmpdev import *
 import matplotlib.pyplot as plt
+from win32api import GetSystemMetrics
 from psychopy import core
 from ctypes import windll
 from PyDAQmx import *
@@ -102,16 +104,13 @@ class triggerThread(threading.Thread):
         self.trigger = 0;
         self.portAddress = 0xD050;
         win32gui.EnumWindows(find_window_callback,".*Spike2 -.*"); #Notepad
+        while whndl==None:
+            self.winNotOpenMessage();
+            win32gui.EnumWindows(find_window_callback,".*Spike2 -.*"); #Notepad
         self.whndl = whndl;
         print self.whndl
         title = str(win32gui.GetWindowText(self.whndl));
         print title
-        #self.whndl = win32gui.FindWindowEx(0,0,None, title);
-        #self.hwnds = {};
-        #win32gui.EnumChildWindows(self.whndl,callback,self.hwnds)
-        #print self.hwnds;
-        #self.whndl = self.hwnds['AfxWnd110u']
-        #self.wnd = win32ui.CreateWindowFromHandle(self.whndl);
         self.app = pywinauto.application.Application();
         self.win = self.app.window_(handle=self.whndl);
         self.shell = win32com.client.Dispatch("WScript.Shell")
@@ -140,6 +139,21 @@ class triggerThread(threading.Thread):
             time.sleep(0.001);
         print "trThread closing";
 
+    def msgbtn(self,i):
+       self.optionChosen = i.text();
+       
+    def winNotOpenMessage(self):
+        msg = QtGui.QMessageBox()
+        msg.setIcon(QtGui.QMessageBox.Question)
+        msg.setFixedSize(250, 100); 
+        msg.move((GetSystemMetrics(0))/2,(GetSystemMetrics(1))/2);
+        msg.setText("Spike 2 window not open")
+        msg.setInformativeText("Please make sure the Spike2 window is open and try again");
+        msg.setWindowTitle("Window Not Open!")
+        msg.setStandardButtons(QtGui.QMessageBox.Ok | QtGui.QMessageBox.Cancel)
+        msg.buttonClicked.connect(self.msgbtn)
+        retval = msg.exec_()
+    
     def close(self):
         self.trigger = -1;
 
@@ -154,6 +168,7 @@ class Form(QtGui.QMainWindow, Ui_MainWindow):
         self.hold.clicked.connect(self.startHoldTask);
         self.learn.clicked.connect(self.startLearnTask);
         self.isometric.clicked.connect(self.startIsometricTask);
+        self.restState.clicked.connect(self.startRestingState);
         self.quit.clicked.connect(self.quit_app);
 
     def timeAfterExercise_str(self,x):
@@ -164,21 +179,26 @@ class Form(QtGui.QMainWindow, Ui_MainWindow):
             3 : '90min',
         }.get(x,'120min')
 
+    def msgbtn(self,i):
+       self.optionChosen = i.text();
+
+    def overwriteMessage(self,filename):
+        msg = QtGui.QMessageBox()
+        msg.setIcon(QtGui.QMessageBox.Question)
+        msg.setFixedSize(250, 100); 
+        msg.move((GetSystemMetrics(0))/2,(GetSystemMetrics(1))/2);
+        msg.setText("Do you want to overwrite an existing file?")
+        msg.setInformativeText("Do you want to overwrite the file "+filename)
+        msg.setWindowTitle("MessageBox demo")
+        msg.setStandardButtons(QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+        msg.buttonClicked.connect(self.msgbtn)
+        retval = msg.exec_()
+        
     def getMaxContrac(self):
         subname = self.subName.text();
         checkFile = "data\\"+subname+"\\"+subname+"_maxContrac.txt";
         if os.path.isfile(checkFile):
-            msg = QtGui.QMessageBox()
-            msg.setIcon(QtGui.QMessageBox.Question)
-            msg.setFixedSize(250, 100); 
-            msg.move((GetSystemMetrics(0))/2,(GetSystemMetrics(1))/2);
-            msg.setText("Do you want to replace the file?")
-            msg.setInformativeText("Do you want to overwrite the file "+checkFile)
-            msg.setWindowTitle("MessageBox demo")
-            msg.setStandardButtons(QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
-            msg.buttonClicked.connect(self.msgbtn)
-            retval = msg.exec_()
-            print self.optionChosen;
+            self.overwriteMessage(checkFile);
             if self.optionChosen == "&No":
                 return;
         global threadNum
@@ -225,6 +245,13 @@ class Form(QtGui.QMainWindow, Ui_MainWindow):
         mutex.release();
         
     def startHoldTask(self):
+        subname = self.subName.text();
+        tAfterExer = self.timeAfterExercise_str(self.combo_holdTime.currentIndex());
+        checkFile = "data\\"+subname+"\\"+subname+"_"+tAfterExer+".txt";
+        if os.path.isfile(checkFile):
+            self.overwriteMessage(checkFile);
+            if self.optionChosen == "&No":
+                return;
         global threadNum
         dThread = dataThread(threadNum,"BIOPAC");
         trThread = triggerThread(threadNum+1,"Trigger");
@@ -253,6 +280,13 @@ class Form(QtGui.QMainWindow, Ui_MainWindow):
         self.obj.plotGripperData(dThread);
 
     def startIsometricTask(self):
+        subname = self.subName.text();
+        tAfterExer = self.timeAfterExercise_str(self.combo_holdTime.currentIndex());
+        checkFile = "data\\"+subname+"\\"+subname+"_"+tAfterExer+"_isometricData.txt";
+        if os.path.isfile(checkFile):
+            self.overwriteMessage(checkFile);
+            if self.optionChosen == "&No":
+                return;
         global threadNum
         dThread = dataThread(threadNum,"BIOPAC");
         trThread = triggerThread(threadNum+1,"Trigger");
@@ -281,6 +315,13 @@ class Form(QtGui.QMainWindow, Ui_MainWindow):
         self.obj.plotGripperData(dThread);
 
     def startLearnTask(self):
+        subname = self.subName.text();
+        block = self.combo_block.currentIndex();
+        checkFile = "data\\"+subname+"_learning\\"+subname+"_learning_Block"+str(block)+".txt";
+        if block!=0 and os.path.isfile(checkFile):
+            self.overwriteMessage(checkFile);
+            if self.optionChosen == "&No":
+                return;
         global threadNum
         dThread = dataThread(threadNum,"BIOPAC");
         trThread = triggerThread(threadNum+1,"Trigger");
@@ -290,7 +331,6 @@ class Form(QtGui.QMainWindow, Ui_MainWindow):
         entered_scoreThresh = int(self.text_scoreThresh.text());
         self.obj = motorLearningTask();
         self.obj.init(str(self.subName.text()));
-        block = self.combo_block.currentIndex();
         if block==6:
             self.combo_fb.setCurrentIndex(0);
         if block==7:
@@ -322,6 +362,26 @@ class Form(QtGui.QMainWindow, Ui_MainWindow):
             self.obj.plotGripperData(dThread, entered_trialTime);
         self.combo_block.setCurrentIndex((1+block)%8);
 
+    def startRestingState(self):
+        global threadNum
+        trThread = triggerThread(threadNum,"Trigger");
+        threadNum = threadNum+1;
+        self.obj = presentCross();
+        self.obj.init();
+        mutex.acquire();
+        trThread.start();
+        mutex.release();
+        totalTime = int(self.text_restTime.text());
+        T_start = time.clock();
+        self.obj.presentCross(totalTime,trThread);
+        T_end = time.clock();
+        print (T_end-T_start);
+        cv2.destroyAllWindows();
+        mutex.acquire();
+        trThread.trigger = -1;
+        trThread.close();
+        mutex.release();
+        
     def quit_app(self):
         os._exit(0);
         exit()
